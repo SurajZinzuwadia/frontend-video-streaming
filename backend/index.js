9e43249be51bfc21724d228b9691c07b8c71f099
Krishna Patel <krishnapatel7046@gmail.com>
2023-08-04 17:56:08 -0400

    removed certificate

--- backend/index.js
+++ backend/index.js
@@ -1,82 +1,63 @@
-const https = require('https');
-const express = require('express')
-const fs = require('fs');
+const express = require('express');
 const cors = require('cors');
 
-const app = express()
-// var certificate  = fs.readFileSync("/app/backend/private.crt");
-// var privateKey = fs.readFileSync("/app/backend/private.key");
-var certificate  = fs.readFileSync("./private.crt");
-var privateKey = fs.readFileSync("./private.key");
+const app = express();
+
 const corsOptions = {
-  origin: ['https://192.168.2.112:3002', 'https://192.168.2.112:3001, https://192.168.2.112:3000'],
+  origin: [
+    'https://192.168.2.112:3002',
+    'https://192.168.2.112:3001',
+    'https://192.168.2.112:3000'
+  ],
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
-app.use(cors(corsOptions));const server = https.createServer(
-  {
-      cert: certificate, 
-      key: privateKey,
-      passphrase: 'dexter'
-  },
-  app
-);
-const io = require('socket.io')(server)
-const { v4: uuidV4 } = require('uuid')
+app.use(cors(corsOptions));
 
-
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const { v4: uuidV4 } = require('uuid');
 
 const { PeerServer } = require('peer');
-const peerServer = PeerServer({ port: 3002, 
-                            path: '/' ,
-                            ssl: {
-                                key: privateKey,
-                                cert: certificate,
-                                passphrase: 'dexter'
-                              }
-                        });
+const peerServer = PeerServer({ port: 3002, path: '/' });
 
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  // console.log("hello 1");
-  res.redirect(`/${uuidV4()}`)
-})
+  res.redirect(`/${uuidV4()}`);
+});
 
 app.get('/:room', (req, res) => {
-  res.render('indexP', { roomId: req.params.room })
-})
+  res.render('indexP', { roomId: req.params.room });
+});
 
 app.get('/j/:room', (req, res) => {
-  console.log(req.params.room )
-  res.render('indexC', { roomId: req.params.room })
-})
+  console.log(req.params.room);
+  res.render('indexC', { roomId: req.params.room });
+});
 
-
-app.get('/join:room', (req, res) => {
-  // console.log("hello 2");
-  // console.log("room id = ", req.params.room );
-
-  res.render('room', { roomId: req.params.room })
-})
+app.get('/join/:room', (req, res) => {
+  res.render('room', { roomId: req.params.room });
+});
 
 io.on('connection', socket => {
   socket.on('GoLive', (roomId, userId) => {
-    console.log('Live Room Created roomId:',roomId, "by User userid: ",userId);
-    socket.join(roomId)
-    // socket.to(roomId).broadcast.emit('Live-started', userId)
+    console.log('Live Room Created roomId:', roomId, "by User userid: ", userId);
+    socket.join(roomId);
 
     socket.on('disconnect', () => {
-      socket.to(roomId).broadcast.emit('Live-disconnected', userId)
-    })
+      socket.to(roomId).broadcast.emit('Live-disconnected', userId);
+    });
+  });
 
-  })
   socket.on('JoinLive', (roomId, userId) => {
-    console.log('Live user connected :',roomId, userId);
+    console.log('Live user connected:', roomId, userId);
     socket.join(roomId);
     socket.to(roomId).broadcast.emit('user-connected', userId);
-  })
-})
+  });
+});
 
-server.listen(8000)
+server.listen(8000, () => {
+  console.log('Server is listening on port 8000');
+});
