de21b0fbe0217e8fdec5c02641f8abb19bbd2ad0
Krishna Patel <krishnapatel7046@gmail.com>
2023-08-04 18:02:41 -0400

    removed IP of GO LIVE

--- frontend/src/pages/HighlightPage.js
+++ frontend/src/pages/HighlightPage.js
@@ -42,7 +42,7 @@ export default function TestingPage() {
       }
     
     // const url = `https://3.210.49.37:8000/${loggedUser._id}`;
-        const url = `http://3.210.49.37:8000/${loggedUser._id}`;
+        const url = `https://www.surajzinzuwadia.com:8001/${loggedUser._id}`;
 
     window.open(url, "_blank");  };
 
