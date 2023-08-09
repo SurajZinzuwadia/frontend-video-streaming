9ba9baa47994deb21032ef09bb62f7d12dbee56c
Krishna Patel <krishnapatel7046@gmail.com>
2023-08-09 12:45:59 -0400

    file changed

--- backend/index.js
+++ backend/index.js
@@ -77,7 +77,7 @@ app.get('/j/:room', (req, res) => {
 });
 
 app.get('/group/:room', (req, res) => {
-  res.render('group', { roomId: req.params.room });
+  res.render('groupcall', { roomId: req.params.room });
 });
 
 io.on('connection', socket => {
